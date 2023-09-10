f93d0e09f1437d7629b3d8994e06dbcd2d05a272
sunilsingh15 <sunilsingh.1996@hotmail.com>
2023-09-10 11:20:19 +0800

    map component initial build with hardcoded values

--- client/src/app/components/map/map.component.ts
+++ client/src/app/components/map/map.component.ts
@@ -1,50 +1,6 @@
-// import { HttpClient } from '@angular/common/http';
-// import { Component, OnInit } from '@angular/core';
-// import { Observable, catchError, map, of } from 'rxjs';
-
-// @Component({
-//   selector: 'app-map',
-//   templateUrl: './map.component.html',
-//   styleUrls: ['./map.component.css']
-// })
-// export class MapComponent {
-
-//   apiLoaded: Observable<boolean>;
-
-//   constructor(http: HttpClient) {
-//       this.apiLoaded = http.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyChsTTTpJbSqI7lhm8-ZhzwcJsKbuV8408', 'callback')
-//         .pipe(
-//           map(() => true),
-//           catchError(() => of(false)),
-//         );
-//   }
-
-//   options: google.maps.MapOptions = {
-//     mapId: 'b155309acfe67a51',
-//     center: { lat: 1.335, lng: 103.8 },
-//     zoom: 11.5,
-//     disableDefaultUI: true
-//   }
-
-//   markerOptions: google.maps.MarkerOptions = {
-//     draggable: false,
-//     // animation: google.maps.Animation.DROP,
-//     icon: {
-//       url: 'assets/cat-marker.png',
-//       // scaledSize: new google.maps.Size(20, 20)
-//     }
-//   }
-
-//   markerPositions: google.maps.LatLngLiteral[] = [
-//     { lat: 1.335, lng: 103.8 },
-//     { lat: 1.335, lng: 103.9 }
-//   ];
-
-// }
-
-import { Component, OnInit } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, of } from 'rxjs';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MapInfoWindow, MapMarker } from '@angular/google-maps';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-map',
@@ -53,30 +9,31 @@ import { Observable, catchError, map, of } from 'rxjs';
 })
 export class MapComponent implements OnInit {
 
-  // constructor(private http: HttpClient) {}
-
   apiLoaded!: Observable<boolean>;
   options!: google.maps.MapOptions;
   markerOptions!: google.maps.MarkerOptions;
   markerPositions!: google.maps.LatLngLiteral[];
+  catName: string = 'Cat';
+
+  @ViewChild(MapInfoWindow)
+  infoWindow!: MapInfoWindow;
 
   ngOnInit(): void {
     this.loadGoogleMapsApi().subscribe(() => {
-      // Google Maps API is loaded and ready
       this.apiLoaded = of(true);
 
-      // Initialize your map and marker options here
       this.options = {
         mapId: 'b155309acfe67a51',
-        center: { lat: 1.335, lng: 103.8 },
+        center: { lat: 1.34, lng: 103.82 },
         zoom: 11.5,
         disableDefaultUI: true
       };
 
       this.markerOptions = {
         draggable: false,
+        animation: google.maps.Animation.DROP,
         icon: {
-          url: 'assets/cat-marker.png',
+          url: 'assets/marker.png',
           scaledSize: new google.maps.Size(20, 20)
         }
       };
@@ -86,10 +43,11 @@ export class MapComponent implements OnInit {
         { lat: 1.335, lng: 103.9 },
         { lat: 1.345, lng: 103.96 },
         { lat: 1.365, lng: 103.72 },
-        { lat: 1.315, lng: 103.69 }
+        { lat: 1.315, lng: 103.69 },
+        {lat: 1.3732241859585121, lng: 103.74611465231567}
       ];
+
     }, () => {
-      // Handle API loading error
       this.apiLoaded = of(false);
     });
   }
@@ -112,5 +70,11 @@ export class MapComponent implements OnInit {
 
       document.body.appendChild(script);
     });
+  }
+
+  openWindow(marker: MapMarker) {
+    console.log(marker.getPosition()?.toJSON());
+
+    this.infoWindow.open(marker);
   }
 }
