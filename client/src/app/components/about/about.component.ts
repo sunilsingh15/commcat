fcc0ec1945b40e174486cd770f27f1a3c2559533
sunilsingh15 <sunilsingh.1996@hotmail.com>
2023-09-10 17:35:41 +0800

    about component completed

--- client/src/app/components/about/about.component.ts
+++ client/src/app/components/about/about.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 
 }
