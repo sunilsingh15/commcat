fce774136772a4fa4348f2f2403f3785b98c21cc
sunilsingh15 <sunilsingh.1996@hotmail.com>
2023-09-13 23:57:30 +0800

    new methods added to controller for auth

--- server/src/main/java/sg/commcat/server/controllers/CatController.java
+++ server/src/main/java/sg/commcat/server/controllers/CatController.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +22,7 @@ import jakarta.json.JsonObject;
 import sg.commcat.server.services.CatService;
 
 @RestController
-@RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "/api/map", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CatController {
 
     @Autowired
@@ -30,19 +33,65 @@ public class CatController {
         return ResponseEntity.ok(service.getCatLocations());
     }
 
+    @GetMapping(path = "/cat/{catId}")
+    public ResponseEntity<String> getCatInfo(@PathVariable String catId) {
+
+        if (service.doesCatExist(catId)) {
+            return ResponseEntity.ok(service.getCatInfo(catId).toString());
+        } else {
+            JsonObject error = Json.createObjectBuilder()
+                    .add("error", "No cat found with ID %s".formatted(catId))
+                    .build();
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error.toString());
+        }
+    }
+
     @GetMapping(path = "/cat")
     public ResponseEntity<String> getCatInfoForWindow(@RequestParam double lat, @RequestParam double lng) {
         return ResponseEntity.ok(service.getCatInfoForWindow(lat, lng).toString());
     }
 
     @PostMapping(path = "/submit", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<String> submitNewCat(@RequestPart String name, @RequestPart String gender, @RequestPart String community,
+    public ResponseEntity<String> submitNewCat(@RequestPart String name, @RequestPart String gender,
+            @RequestPart String community,
             MultipartFile picture, @RequestPart(required = false) String likes,
             @RequestPart(required = false) String dislikes, @RequestPart(required = false) String personality,
             @RequestPart(required = false) String other) {
 
-                JsonObject response = Json.createObjectBuilder()
-                .add("success", service.processCatSubmission(name, gender, community, picture, likes, dislikes, personality, other))
+        JsonObject response = Json.createObjectBuilder()
+                .add("success",
+                        service.processCatSubmission(name, gender, community, picture, likes, dislikes, personality,
+                                other))
+                .build();
+
+        return ResponseEntity.ok(response.toString());
+    }
+
+    @GetMapping(path = "/submissions")
+    public ResponseEntity<List<Document>> getCatSubmissions() {
+        return ResponseEntity.ok(service.getCatSubmissions());
+    }
+
+    @PostMapping(path = "/approve")
+    public ResponseEntity<String> approveSubmission(@RequestParam String id) {
+
+        service.approveSubmission(id);
+
+        JsonObject response = Json.createObjectBuilder()
+                .add("success", "Cat %s has been approved.".formatted(id))
+                .build();
+
+        return ResponseEntity.ok(response.toString());
+    }
+
+    @DeleteMapping(path = "/reject")
+    public ResponseEntity<String> rejectSubmission(@RequestParam String id) {
+
+        service.rejectSubmission(id);
+
+        JsonObject response = Json.createObjectBuilder()
+                .add("success", "Cat %s has been rejected.".formatted(id))
                 .build();
 
         return ResponseEntity.ok(response.toString());
