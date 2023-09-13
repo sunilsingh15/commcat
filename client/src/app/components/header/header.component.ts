83feaec0fb445a67dc5793fe135196e0f54d0657
sunilsingh15 <sunilsingh.1996@hotmail.com>
2023-09-13 23:33:11 +0800

    header component refactored to work with auth

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { initFlowbite } from 'flowbite';
+import { Subscription } from 'rxjs';
+import { CommcatService } from 'src/app/commcat.service';
+import { StorageService } from 'src/app/storage.service';
 
 @Component({
   selector: 'app-header',
@@ -8,8 +12,38 @@ import { initFlowbite } from 'flowbite';
 })
 export class HeaderComponent implements OnInit {
 
+  isAdminLoggedIn: boolean = StorageService.isAdminLoggedIn();
+  isUserLoggedIn: boolean = StorageService.isUserLoggedIn();
+  service = inject(CommcatService);
+  dashSub$: Subscription;
+  submissions: number = 0;
+
+  router = inject(Router);
+
   ngOnInit(): void {
     initFlowbite();
+
+    this.router.events.subscribe(event => {
+      if (event.constructor.name === "NavigationEnd") {
+        this.isAdminLoggedIn = StorageService.isAdminLoggedIn();
+        this.isUserLoggedIn = StorageService.isUserLoggedIn();
+      }
+    })
+
+    this.dashSub$ = this.service.getSubmissions().subscribe({
+      next: (result) => {
+        if (result != null) {
+          this.submissions = result.length;
+        }
+       },
+      error: (err) => { console.log(err); },
+      complete: () => { this.dashSub$.unsubscribe(); }
+    });
+  }
+
+  logout() {
+    StorageService.logout();
+    this.router.navigate(['/']);
   }
 
 }
